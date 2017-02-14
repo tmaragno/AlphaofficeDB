2a48b6084286f866522d7211f8e72194ce6f8b2c
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-14 11:14:33 -0800

    updates

--- executeremote.sh
+++ executeremote.sh
@@ -2,9 +2,9 @@
 
 mysqlcs_ip=$1
 
-scp -o StrictHostKeyChecking=no -i CustomerKey cr_db.sql opc@$mysqlcs_ip:/home/opc/
+scp -o StrictHostKeyChecking=no -i mysql_private.ppk cr_db.sql opc@$mysqlcs_ip:/home/opc/
 
-ssh -o StrictHostKeyChecking=no -i CustomerKey opc@$mysqlcs_ip <<'ENDSSH'
+ssh -o StrictHostKeyChecking=no -i mysql_private.ppk opc@$mysqlcs_ip <<'ENDSSH'
 chmod 711 /home/opc
 sudo su oracle
 mysql AlphaofficeDB < cr_db.sql
