e61cf502df4dd566ce79db3718c4d0fbe833745c
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-16 12:12:37 -0800

    Updates

---
+++ src/main/resources/db/setup/add_key.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+mysqlcs_ip=$1
+#
+scp -o StrictHostKeyChecking=no -i /src/main/resources/db/setup/mysqlkey /src/main/resources/db/setup/mysqlkey.pub opc@$mysqlcs_ip:.
+#
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c mv /home/opc/mysqlkey.pub ~oracle/.ssh/.
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/mysqlkey.pub
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c cat ~oracle/.ssh/authorized_keys ~oracle/.ssh/mysqlkey.pub > ~oracle/.ssh/authorized_keys_tmp
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/authorized_keys_tmp
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c mv ~oracle/.ssh/authorized_keys_tmp ~oracle/.ssh/authorized_keys
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c rm ~oracle/.ssh/mysqlkey.pub
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c cat /etc/sudoers /home/opc/oracleSudo.txt > /tmp/oracleSudo
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c cp /tmp/oracleSudo /etc/sudoers
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chown root:root /etc/sudoers
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chmod 440 /etc/sudoers
