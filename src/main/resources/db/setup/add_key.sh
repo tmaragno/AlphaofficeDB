06891c6ed8080028805e8cc889e11133c992d08a
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-16 13:37:16 -0800

    Updates

--- src/main/resources/db/setup/add_key.sh
+++ src/main/resources/db/setup/add_key.sh
@@ -7,7 +7,7 @@ scp -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey src/main
 ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip "chmod a+r mysqlkey.pub
 mv mysqlkey.pub /tmp"
 #
-ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c "mv /home/opc/mysqlkey.pub ~oracle/.ssh/.
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c "mv /tmp/mysqlkey.pub ~oracle/.ssh/.
 chown oracle:oinstall ~oracle/.ssh/mysqlkey.pub
 cat ~oracle/.ssh/authorized_keys ~oracle/.ssh/mysqlkey.pub > ~oracle/.ssh/authorized_keys_tmp
 chown oracle:oinstall ~oracle/.ssh/authorized_keys_tmp
