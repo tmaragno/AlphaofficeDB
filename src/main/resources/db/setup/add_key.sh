6b674ab6f851e53e8e18b5fe109351742f29d777
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-16 13:40:50 -0800

    Updates

--- src/main/resources/db/setup/add_key.sh
+++ src/main/resources/db/setup/add_key.sh
@@ -8,12 +8,12 @@ ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mys
 mv mysqlkey.pub /tmp"
 #
 ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c "mv /tmp/mysqlkey.pub ~oracle/.ssh/.
-chown oracle:oinstall ~oracle/.ssh/mysqlkey.pub
-cat ~oracle/.ssh/authorized_keys ~oracle/.ssh/mysqlkey.pub > ~oracle/.ssh/authorized_keys_tmp
-chown oracle:oinstall ~oracle/.ssh/authorized_keys_tmp
-mv ~oracle/.ssh/authorized_keys_tmp ~oracle/.ssh/authorized_keys
-rm ~oracle/.ssh/mysqlkey.pub
-cat /etc/sudoers /home/opc/oracleSudo.txt > /tmp/oracleSudo
-cp /tmp/oracleSudo /etc/sudoers
-chown root:root /etc/sudoers
-chmod 440 /etc/sudoers"
+sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/mysqlkey.pub
+sudo su - oracle -c cat ~oracle/.ssh/authorized_keys ~oracle/.ssh/mysqlkey.pub > ~oracle/.ssh/authorized_keys_tmp
+sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/authorized_keys_tmp
+sudo su - oracle -c mv ~oracle/.ssh/authorized_keys_tmp ~oracle/.ssh/authorized_keys
+sudo su - oracle -c rm ~oracle/.ssh/mysqlkey.pub
+sudo su - oracle -c cat /etc/sudoers /home/opc/oracleSudo.txt > /tmp/oracleSudo
+sudo su - oracle -c cp /tmp/oracleSudo /etc/sudoers
+sudo su - oracle -c chown root:root /etc/sudoers
+sudo su - oracle -c chmod 440 /etc/sudoers"
