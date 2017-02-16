562d32e71bcbb6b77360b4488a12bfae4d02602f
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-15 16:54:09 -0800

    updates

--- src/main/resources/db/setup/populate_db.sh
+++ src/main/resources/db/setup/populate_db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -i mysqlkey opc@140.86.0.66 "./testexpect2.sh"
+mysqlcs_ip=$1
+scp -o StrictHostKeyChecking=no -i /home/dcameron/gitrepo/AlphaofficeDB/src/main/resources/db/setup/mysqlkey populate_db.sql opc@$mysqlcs_ip:.
+ssh -o StrictHostKeyChecking=no -i /home/dcameron/gitrepo/AlphaofficeDB/src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c mysql AlphaofficeDB<populate_db.sql
 echo "..done."
