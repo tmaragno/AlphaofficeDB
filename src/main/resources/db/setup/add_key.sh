f7b948356b23e5128f94ffd02ba4ec68aec8d68a
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-16 13:52:29 -0800

    Updates

--- src/main/resources/db/setup/add_key.sh
+++ src/main/resources/db/setup/add_key.sh
@@ -7,13 +7,13 @@ scp -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey src/main
 ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip "chmod a+r mysqlkey.pub
 mv mysqlkey.pub /tmp/."
 #
-ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c "mv /tmp/mysqlkey.pub ~oracle/.ssh/.
-sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/mysqlkey.pub
-sudo su - oracle -c cat ~oracle/.ssh/authorized_keys ~oracle/.ssh/mysqlkey.pub > ~oracle/.ssh/authorized_keys_tmp
-sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/authorized_keys_tmp
-sudo su - oracle -c mv ~oracle/.ssh/authorized_keys_tmp ~oracle/.ssh/authorized_keys
-sudo su - oracle -c rm ~oracle/.ssh/mysqlkey.pub
-sudo su - oracle -c cat /etc/sudoers /home/opc/oracleSudo.txt > /tmp/oracleSudo
-sudo su - oracle -c cp /tmp/oracleSudo /etc/sudoers
-sudo su - oracle -c chown root:root /etc/sudoers
-sudo su - oracle -c chmod 440 /etc/sudoers"
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c mv /tmp/mysqlkey.pub ~oracle/.ssh/.
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/mysqlkey.pub
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c cat ~oracle/.ssh/authorized_keys ~oracle/.ssh/mysqlkey.pub > ~oracle/.ssh/authorized_keys_tmp
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chown oracle:oinstall ~oracle/.ssh/authorized_keys_tmp
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c mv ~oracle/.ssh/authorized_keys_tmp ~oracle/.ssh/authorized_keys
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c rm ~oracle/.ssh/mysqlkey.pub
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c cat /etc/sudoers /home/opc/oracleSudo.txt > /tmp/oracleSudo
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c cp /tmp/oracleSudo /etc/sudoers
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chown root:root /etc/sudoers
+ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c chmod 440 /etc/sudoers
