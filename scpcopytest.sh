7183709ebf2ffe8d6773986cd05d8ff7c3600d25
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-17 10:10:32 -0800

    Updates

--- scpcopytest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dbcs_ip=$1
-
-echo "--//--"
-scp -o StrictHostKeyChecking=no -i CustomerKey cr_db.sql opc@$dbcs_ip:/home/opc/
-ssh -o StrictHostKeyChecking=no -i CustomerKey opc@$dbcs_ip "cr_db.sh"
-echo "..done."
