e06b8765dada8e396f2f3a5b949e54d2a2894680
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-22 08:40:56 -0800

    Show John

--- src/main/resources/db/setup/populate_db.sh
+++ src/main/resources/db/setup/populate_db.sh
@@ -26,3 +26,4 @@ mysqlcs_ip="$(tr -d "\"\`'[:alpha:]:" <<<$response)"
 # scp -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey /src/main/resources/db/setup/populate_db.sql opc@$mysqlcs_ip:.
 ssh -o StrictHostKeyChecking=no -i src/main/resources/db/setup/mysqlkey opc@$mysqlcs_ip sudo su - oracle -c mysql AlphaofficeDB<src/main/resources/db/setup/populate_db.sql
 echo "..done."
+
