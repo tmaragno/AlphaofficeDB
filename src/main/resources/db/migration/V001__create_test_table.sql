aec9b616965bbf8cdcec8f6f215a8be3acdff323
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-23 13:14:57 -0800

    Initialize

--- src/main/resources/db/migration/V001__create_test_table.sql
+++
@@ -1,5 +0,0 @@
-/* Single line comment */
-CREATE TABLE test_data (
- value VARCHAR(25) NOT NULL,
- PRIMARY KEY(value)
-);
--- src/main/resources/db/migration/V002__add_test_data.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE AddData()
- BEGIN
-   # MySQL-style single line comment
-   INSERT INTO test_data (value) VALUES ('Hello');
- END //
-DELIMITER;
-
-CALL AddData();
