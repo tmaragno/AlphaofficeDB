941184d46c5b79544f26558631d2b2d4e6765e78
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-02-28 15:54:44 -0800

    Updates

--- src/main/resources/db/migration/V002__AlphaofficeDB_twitterapp.sql
+++ src/main/resources/db/migration/V002__AlphaofficeDB_twitterapp.sql
@@ -1,5 +1,3 @@
-use AlphaofficeDB;
-
 alter table PRODUCTS add column `TWITTER_TAG` varchar(1000) after `EXTERNAL_URL`;
 
 update PRODUCTS set TWITTER_TAG=concat('#',SUBSTRING_INDEX(`PRODUCT_NAME`, ' ', 1));
