70242a759ab45a5f8682d0ca11322db2996997d4
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-05-24 13:32:50 -0700

    update

--- src/main/resources/db/setup/cr_mysql_clean3.sh
+++ src/main/resources/db/setup/cr_mysql_clean3.sh
@@ -15,16 +15,6 @@ echo "REST Endpoint: ${PAAS_HOST}"
 
 # ---------------------------- CREATE TEST INSTANCE ------------------------------------
 
-#Check if Database Already Exists
-echo '\n[info] Checking DB existance\n'
-let httpCode=`curl -i -X GET \
- -u "${USER_ID}:${USER_PASSWORD}" \
- -H "X-ID-TENANT-NAME: ${ID_DOMAIN}" \
- -H "Accept: application/json" \
- -sL -w "%{http_code}" \
-  https://${PAAS_HOST}/paas/api/v1.1/instancemgmt/${ID_DOMAIN}/services/MySQLCS/instances/AlphaofficeDB \
-  -o /dev/null`
-
 	# submit job and take result of job submission, strip spaces and assign to variable job
 	job=$(curl -X POST \
 	-u "${USER_ID}:${USER_PASSWORD}" \
@@ -65,14 +55,3 @@ let httpCode=`curl -i -X GET \
 	echo $result
 	echo $(date)
 
-# Open port 1521
-
-curl -i -X POST \
-  -u "${USER_ID}:${USER_PASSWORD}" \
-  -H "X-ID-TENANT-NAME: ${ID_DOMAIN}" \
-  -d '{"ruleName":"open1521","ruleType":"USER","description":"","source":"PUBLIC-INTERNET","destination":"mysql_MASTER","ports":"1521","protocol":"tcp","status":"enabled"}' \
-  -H "Accept: application/json" \
-  -H "Content-Type: application/json" \
-https://${PAAS_HOST}/paas/api/v1.1/instancemgmt/${ID_DOMAIN}/services/MySQLCS/instances/AlphaofficeDB/accessrules
-
-echo '\n[info] Deployment complete\n'
