ff84efe48fb647dfe3dcfed01c13f5b3d0667f46
Derrick Cameron <dcameron_cdn@yahoo.ca>
2017-05-24 11:02:42 -0700

    update

--- src/main/resources/db/setup/cr_mysql_clean2.sh
+++ src/main/resources/db/setup/cr_mysql_clean2.sh
@@ -15,16 +15,6 @@ echo "REST Endpoint: ${PAAS_HOST}"
 
 # ---------------------------- CREATE TEST INSTANCE ------------------------------------
 
-#Check if Database Already Exists
-echo '\n[info] Checking DB existance\n'
-let httpCode=`curl -i -X GET \
- -u "${USER_ID}:${USER_PASSWORD}" \
- -H "X-ID-TENANT-NAME: ${ID_DOMAIN}" \
- -H "Accept: application/json" \
- -sL -w "%{http_code}" \
-  https://${PAAS_HOST}/paas/api/v1.1/instancemgmt/${ID_DOMAIN}/services/MySQLCS/instances/AlphaofficeDB \
-  -o /dev/null`
-
 	# submit job and take result of job submission, strip spaces and assign to variable job
 	job=$(curl -X POST \
 	-u "${USER_ID}:${USER_PASSWORD}" \
